d124245d4031115bac406313b70a2701943b3ad4
mehdi <dehghani_m@rocketmail.com>
2022-07-07 21:23:56 +0430

    start make db room

---
+++ app/src/main/java/com/example/newsappapi/db/ArticleDao.kt
@@ -0,0 +1,20 @@
+package com.example.newsappapi.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.newsappapi.Article
+
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article) : Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles() : LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
