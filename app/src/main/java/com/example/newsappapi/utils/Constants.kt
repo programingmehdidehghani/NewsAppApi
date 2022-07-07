a83d30343c0b4ec81305f6a2dc0ea9a8107f50d4
mehdi <dehghani_m@rocketmail.com>
2022-07-07 21:11:08 +0430

    make RetrofitInstance

--- app/src/main/java/com/example/newsappapi/utils/Constants.kt
+++ app/src/main/java/com/example/newsappapi/utils/Constants.kt
@@ -3,6 +3,7 @@ package com.example.newsappapi.utils
 class Constants {
     companion object{
         const val API_KEY = "7edf22d3e2fb4dcb85b414daf7e924f4"
+        const val BASE_URL = "https://newsapi.org"
 
     }
 }
