6f6de3268b6f067775dfa3088e0c249defa8bf8e
mehdi <dehghani_m@rocketmail.com>
2022-07-14 11:01:05 +0430

    continue news app

--- app/src/main/java/com/example/newsappapi/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsappapi/repository/NewsRepository.kt
@@ -1,5 +1,6 @@
 package com.example.newsappapi.repository
 
+import androidx.room.Query
 import com.example.newsappapi.api.RetrofitInstance
 import com.example.newsappapi.db.ArticleDatabase
 
@@ -9,4 +10,7 @@ class NewsRepository(
 
     suspend fun getBreakingNews(countryCode : String , pageNumber : Int) =
         RetrofitInstance.api.getBreakingNews(countryCode,pageNumber)
+
+    suspend fun searchNews(searchQuery : String ,pageNumber: Int ) =
+        RetrofitInstance.api.getSearchForNews(searchQuery,pageNumber)
 }
--- app/src/main/java/com/example/newsappapi/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsappapi/ui/fragments/BreakingNewsFragment.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsappapi.R
 import com.example.newsappapi.adapters.NewsAdapter
+import com.example.newsappapi.db.ArticleDatabase
+import com.example.newsappapi.repository.NewsRepository
 import com.example.newsappapi.ui.NewsActivity
 import com.example.newsappapi.ui.NewsViewModel
+import com.example.newsappapi.ui.NewsViewModelProviderFactory
 import com.example.newsappapi.utils.Resource
 import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
@@ -22,7 +26,9 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = (activity as NewsActivity).viewModel
+        val newsRepository = NewsRepository(ArticleDatabase(view.context))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         setupRecyclerView()
         viewModel.breakingNews.observe(viewLifecycleOwner , Observer { response ->
              when(response){
